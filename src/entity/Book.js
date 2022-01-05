3f578c8f068488c0e8a67d330ad7921569174ebb
MuizU <muizuvais99@gmail.com>
2022-01-05 22:36:09 +0530

    Changed the id decorator of the book and the type

--- src/entity/Book.js
+++ src/entity/Book.js
@@ -15,8 +15,8 @@ var Book = /** @class */ (function () {
     function Book() {
     }
     __decorate([
-        (0, typeorm_1.PrimaryGeneratedColumn)(),
-        __metadata("design:type", Number)
+        (0, typeorm_1.PrimaryGeneratedColumn)("uuid"),
+        __metadata("design:type", String)
     ], Book.prototype, "id", void 0);
     __decorate([
         (0, typeorm_1.Column)(),
--- src/entity/Book.ts
+++ src/entity/Book.ts
@@ -2,8 +2,8 @@ import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Book {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
   @Column()
   title: string;
